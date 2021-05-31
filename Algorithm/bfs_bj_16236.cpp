cd6ba9f3e7ff84db3fd7d0f43116cb75d3afdb86
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2021-05-31 23:59:35 +0900

    Add files via upload

---
+++ Algorithm/bfs_bj_16236.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#define MAX 20
+
+int arr[MAX][MAX];
+
+int main(void)
+{
+	
+}
+//https://www.acmicpc.net/problem/16236
+/*
+접근 아기상어의 크기와 좌표를 동시에 저장해야 될것 같음
+가장 가까이에 있는 물고기를 먹는데 어떤 크기에 따라 먹는지도 정확한 이해가 필요
+*/
