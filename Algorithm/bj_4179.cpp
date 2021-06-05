7aec5ea8fdc12531baa4f94b38c8b5c7860c07db
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2021-06-05 15:18:53 +0900

    Update bj_4179.cpp

--- Algorithm/bj_4179.cpp
+++ Algorithm/bj_4179.cpp
@@ -1,10 +1,44 @@
 #include<iostream>
+#include<queue>
 #define MAX 1000
 using namespace std;
 
 int R,C;
 int field[MAX][MAX];
+int visited[MAX][MAX];
 
+int dx[4] = { -1,0,0,1 };
+int dy[4] = { 0,-1,1,0 };
+
+queue<pair<int, int>> q;
+
+void bfs() {
+	q.push({ jx,jy });
+	visit[jx][jy] = 1;
+
+	while (!q.empty()) {
+		int x = q.front().first;
+		int y = q.front().second;
+		q.pop();
+
+		if (p[x][y] == 0 && (x == 1 || x == N || y == 1 || y == M)) {
+			res = visit[x][y];
+			return;
+		}
+
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (nx<1 || ny < 1 || nx>N || ny>M) continue;
+			if (p[nx][ny] == 1) continue;
+			if (visit[nx][ny] == -1) {
+				visit[nx][ny] = visit[x][y] + 1;
+				p[nx][ny] = p[x][y];
+				q.push({ nx,ny });
+			}
+		}
+	}
+}
 void Input()
 {
 	cin >> R >> C;
@@ -30,4 +64,4 @@ int main(void){
 
 }
 //https://www.acmicpc.net/problem/4179
-//¿ì¼±Å¥¿¡ ³Ö¾î¾ß ÇÒ °ÍÀ» Ã£¾Æ¾ßÇÔ
+//ìš°ì„ íì— ë„£ì–´ì•¼ í•  ê²ƒì„ ì°¾ì•„ì•¼í•¨
