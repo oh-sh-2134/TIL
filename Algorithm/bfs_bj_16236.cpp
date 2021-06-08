4cf23ed0b973ad824f816ca8eb1aa55e65309fa9
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2021-06-08 23:48:10 +0900

    Update bfs_bj_16236.cpp

--- Algorithm/bfs_bj_16236.cpp
+++ Algorithm/bfs_bj_16236.cpp
@@ -1,7 +1,61 @@
 #include<iostream>
-#define MAX 20
+#include<queue>
+#include<vector>
+#include<cstring>
+#include<algorithm>
 
+#define endl "\n"
+#define MAX 20
+using namespace std;
+
+typedef struct
+{
+	int x;
+	int y;
+	int Size;
+	int Eat_Num;
+	int Time;
+}Shark;
+
+typedef struct
+{
+	int x;
+	int y;
+	int Dist;
+}Food;
+
+
+int N;
 int arr[MAX][MAX];
+bool visited[MAX][MAX];
+
+Shark S;
+vector<Food> V;
+
+int dx[] = { 0, 0, 1, -1 };
+int dy[] = { 1 ,-1 ,0 ,0 };
+
+
+void Input()
+{
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cin >> MAP[i][j];
+			if (MAP[i][j] == 9)
+			{
+				S.x = i;
+				S.y = j;
+				S.Size = 2;
+				S.Eat_Num = 0;
+				S.Time = 0;
+			}
+		}
+	}
+}
+
 
 int main(void)
 {
@@ -11,6 +65,9 @@ int main(void)
 /*
 접근 아기상어의 크기와 좌표를 동시에 저장해야 될것 같음
 가장 가까이에 있는 물고기를 먹는데 어떤 크기에 따라 먹는지도 정확한 이해가 필요
->> 아이디어를 참고 해보니 우선큐에 대한 이해가 필요함
-https://www.acmicpc.net/problem/4179     4179번 문제를 먼저 연습해야 함
+
+0608
+너무 어려워서 설명을 보고 클론 코딩...
+https://yabmoons.tistory.com/160
+
 */
