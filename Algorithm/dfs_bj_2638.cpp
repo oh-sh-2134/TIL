4d0f50f71d0f8991bda1339fe6489e260475c319
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2021-07-02 13:11:09 +0900

    Update dfs_bj_2638.cpp

--- Algorithm/dfs_bj_2638.cpp
+++ Algorithm/dfs_bj_2638.cpp
@@ -13,7 +13,7 @@ int dx[4] = { 0,0,1,-1 };
 int dy[4] = { 1,-1,0,0 };
 int m, n, cnt;
 int arr[MAX][MAX];
-bool visted[MAX][MAX];
+bool visited[MAX][MAX];
 
 
 void Input()
@@ -28,35 +28,12 @@ void Input()
 	}
 }
 
-bool CheesCheck()
-{
-	bool flag = false;
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; j++)
-		{
-			if (arr[i][j] < Cheese || arr[i][j] >= MeltCheese)
-			{
-				arr[i][j] = Air;
-				visted[i][j] = false;
-			}
-			else 
-			{
-				flag = true;
-				arr[i][j] = Cheese;
-				visted[i][j] = false;
-			}
-		}
-	}
-	return flag;
-}
-
-void Airing()
+void Airing(int a, int b)
 {
 	queue<pair<int, int>>q;
-	q.push(make_pair(0, 0));
-	arr[0][0] = MeltedAir;
-
+	q.push(make_pair(a, b));
+	arr[a][b] = MeltedAir;
+	visited[a][b] = true;
 	while (!q.empty())
 	{
 		int y = q.front().first;
@@ -68,10 +45,10 @@ void Airing()
 			int nx = x + dx[i];
 
 			if (x < 0 || y < 0 || x >= m || y >= n) continue;
-			if (arr[ny][nx] == Air && !visted[ny][nx])
+			if (arr[ny][nx] == Air && !visited[ny][nx])
 			{
 
-				visted[ny][nx] = true;
+				visited[ny][nx] = true;
 				arr[ny][nx] = MeltedAir;
 				q.push(make_pair(ny,nx));
 			}
@@ -81,19 +58,69 @@ void Airing()
 
 }
 
+bool CheesCheck()
+{
+	bool flag = false;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (arr[i][j] >= MeltCheese)
+			{
+				visited[i][j] = false;
+				arr[i][j] = MeltedAir;
+				Airing(i,j);
+			}
+			else if (arr[i][j] >=Cheese)
+			{
+				flag = true;
+				arr[i][j] = Cheese;
+				visited[i][j] = false;
+			}
+		}
+	}
+	return flag;
+}
+
+
+
 void dfs(int a, int b)
 {
-	if (arr[a][b] >= Cheese && !visted[a][b])
+	if (arr[a][b] >= Cheese && !visited[a][b])
 	{
-		visted[a][b] = true;
+		visited[a][b] = true;
 		for (int i = 0; i < 4; i++)
 		{
 			int y = a + dy[i];
 			int x = b + dx[i];
 			if (x < 0 || y < 0 || x >= m || y >= n) continue;
 			if (arr[y][x] == MeltedAir) arr[a][b]++;
-			if (arr[y][x] >=Cheese) 
+			if (arr[y][x] >=Cheese && !visited[y][x]) 
 				dfs(y, x);
+		}
+	}
+
+}
+
+void bfs(int a, int b)
+{
+	visited[a][b] = true;
+	queue<pair<int, int>>q;
+	q.push(make_pair(a, b));
+	while(!q.empty())
+	{
+		int y = q.front().first;
+		int x = q.front().second;
+		q.pop();
+		for(int i=0;i<4;i++)
+		{
+			if (x < 0 || y < 0 || x >= m || y >= n) continue;
+			if (arr[y][x] == MeltedAir) arr[a][b]++;
+			if (arr[y][x] >=Cheese && !visited[y][x])
+			{
+				visited[y][x] = true;
+				q.push(make_pair(y,x));
+			}
 		}
 	}
 
@@ -103,15 +130,15 @@ void dfs(int a, int b)
 void solution()
 {
 	int sol = 0;
+	Airing(0,0);
 	while (CheesCheck())
 	{
 		sol++;
-		Airing();
 		for (int i = 0; i < n; i++)
 		{
 			for (int j = 0; j < m; j++)
 			{
-				if (arr[i][j] == Cheese && !visted[i][j])
+				if (arr[i][j] == Cheese && !visited[i][j])
 					dfs(i, j);
 			}
 		}
@@ -126,8 +153,10 @@ int main(void)
 	solution();
 }
 
+
 //https://www.acmicpc.net/problem/2638
 //이전 치즈 문제랑 차이점은 공기에 닿는 면이 2개 이상일때 녹는다는 점
 //현제 녹는 공기를 정의하는 함수, 치즈가 존재하는지 확인하는 함수 만듬 dfs와 solution함수 수정해야함
 //시간초과 나옴 원인 파악필요
 //20210701 에어링을 최초 1회후 녹은 치즈 기준으로만 돌리면 될 것 같음, 오답처리되는 원인을 찾아야함
+//20210702 고민했었던 방법대로 했지만 9퍼에서 시간초과 되어 bfs도 만들어봤는데 마찬가지 연산 횟수를 줄이는 방법을 생각해야함
