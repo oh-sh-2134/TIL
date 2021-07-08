decec1d4f2a325204857bdd4856f975c88505a0d
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2021-07-08 13:10:18 +0900

    Update dfs_bj_9446.cpp

--- Algorithm/dfs_bj_9446.cpp
+++ Algorithm/dfs_bj_9446.cpp
@@ -4,17 +4,32 @@
 using namespace std;
 
 int arr[MAX];
+bool visited[MAX];
+bool done[MAX];
 int T, n, cnt;
 
-void dfs()
+void dfs(int Current)
 {
+	visited[Current] = true;
+	int Next = arr[Current];
+	if(!visited[Next])
+		dfs(arr[Next]);
+	else if(!done[Current])
+		for(int i = Next; i != Current; i = arr[Next])
+			cnt++;
+	cnt++;
 
+	done[Current] = true;
 }
 
 void solution()
 {
-	dfs();
-	cout << cnt;
+	for(int i = 1; i<=n; i++)
+	{
+		if(!visited[i])
+			dfs(1);
+	}
+	cout << n - cnt;
 }
 
 void Input()
@@ -26,9 +41,15 @@ void Input()
 		for (int i = 1; i <= n; i++)
 		{
 			cin >> arr[i];
+			visited[i] = false;
+			done[i] = false;
 		}
 		solution();
 	}
 }
+
+int main(void){
+	Input();
+}
 //https://www.acmicpc.net/problem/9466
-//dfs¸¦ µ¹±äÇØ¾ßÇÏ´Âµ¥ ¾ÆÀÌµğ¾î ¾È¶°¿À¸§
+//dfsë¥¼ ëŒê¸´í•´ì•¼í•˜ëŠ”ë° ì•„ì´ë””ì–´ ì•ˆë– ì˜¤ë¦„
