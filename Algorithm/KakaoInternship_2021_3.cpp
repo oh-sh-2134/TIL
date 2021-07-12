33ba40d9196754ca9fc52830833e9757cc637b58
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2021-07-12 23:20:20 +0900

    Update KakaoInternship_2021_3.cpp

--- Algorithm/KakaoInternship_2021_3.cpp
+++ Algorithm/KakaoInternship_2021_3.cpp
@@ -1,5 +1,48 @@
 #include <string>
 #include <vector>
+#include <set>
+#include <iostream>
+//#include<bits/stdc++.h>
+using namespace std;
+
+string solution(int n, int k, vector<string> cmd) {
+	string answer(n, 'X');
+	set<int> s;
+	vector<int> erased;
+	for (int i = 0; i < n; i++) {
+		s.insert(i);
+	}
+	auto it = s.find(k);
+	for (auto c : cmd) {
+		if (c[0] == 'U') {
+			int num = stoi(c.substr(2));
+			while (num--) it--;
+		}
+		else if (c[0] == 'D') {
+			int num = stoi(c.substr(2));
+			while (num--) it++;
+		}
+		else if (c[0] == 'C') {
+			erased.push_back(*it);
+			it = s.erase(it);
+			if (it == s.end()) it--;
+		}
+		else {
+			s.insert(erased.back());
+			erased.pop_back();
+		}
+	}
+	for (auto x : s) answer[x] = 'O';
+	return answer;
+
+}
+int main(void) {
+	vector<string> s = { "D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C" };
+	cout << solution(8, 2, s);
+}
+/*
+#include <string>
+#include <vector>
 #include <iostream>
 #include <stack>
 using namespace std;
@@ -58,3 +101,5 @@ int main(void) {
 	vector<string> s = { "D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C" };
 	cout << solution(8, 2, s);
 }
+*/
+//분석필요
