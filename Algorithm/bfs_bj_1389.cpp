604413cdbd630a2699226745089956614d8ed884
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2021-07-19 23:06:46 +0900

    Update bfs_bj_1389.cpp

--- Algorithm/bfs_bj_1389.cpp
+++ Algorithm/bfs_bj_1389.cpp
@@ -72,5 +72,93 @@ int main(void)
 	Input();
 	solution();
 }
+/*
+#include<iostream>
+#define MAX 101
+#define INF 987654321
+using namespace std;
+
+int node[MAX][MAX];
+int visited[MAX];
+int temp[MAX];
+
+int n, m;
+
+
+void Floyd_Warshall()
+{
+	for (int k = 1; k <= n; k++)
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			for (int j = 1; j <= n; j++)
+			{
+				if (i == j) continue;
+				if (node[i][j] > node[i][k] + node[k][j]) {
+					node[i][j] = node[i][k] + node[k][j];
+				}
+				
+			}
+		}
+	}
+
+
+}
+
+void MakeTable() 
+{
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			if (i == j) 
+				node[i][j] = 0;
+			else 
+				node[i][j] = INF;
+		}
+	}
+}
+
+void Input() {
+	cin >> n >> m;
+	MakeTable();
+	int a, b;
+	for (int i = 0; i < m; i++)
+	{
+		cin >> a >> b;
+		node[a][b] = 1;
+		node[b][a] = 1;
+	}
+}
+
+void solution() {
+	int sol,min = INF;
+	Floyd_Warshall();
+	for (int i = 1; i <= n; i++)
+	{
+		int sum = 0;
+		for (int j = 1; j <= n; j++)
+		{
+			if (i == j) continue;
+			sum += node[i][j];
+		}
+		cout << temp[i] << "\n";
+		if (min > sum)
+		{
+			min = sum;
+			sol = i;
+		}
+	}
+	cout << sol;
+}
+
+int main(void) 
+{
+	Input();
+	solution();
+}
+*/
 //https://www.acmicpc.net/problem/1389
-//BFS를 사용하여서 풀었지만 케빈베이컨 알고리즘을 이용하여 한번더 풀어볼 것.
+//BFS를 사용하여서 풀었지만 플로이드 와샬 알고리즘을 이용하여 한번더 풀어볼 것.
+//플로이드 와샬 - 모든노드가 모든 노드를 한번씩 방문하는 최단거리를 구하는 공식
+//A -> B로 갈때 A-> C , C-> B 로가는게 더 짧을 가능성을 찾는 알고리즘
