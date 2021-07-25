5fb9de9f44fc89e2ea351bc0bd487f7b70b151a6
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2021-07-25 23:21:05 +0900

    Update BOJ_14496.cpp

--- Algorithm/BOJ_14496.cpp
+++ Algorithm/BOJ_14496.cpp
@@ -1,81 +1,66 @@
-#include<iostream>
-#define MAX 1001
-#define INF 987654321
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#define INF 1000000000
 using namespace std;
+ 
+vector<pair<int, int> > list[1001];
+int d[1001];
+int N, M, start, des;
+ 
+void dijkstra() {
+    d[start] = 0;
+    priority_queue<pair<int, int> > q;
+    q.push({ start, 0 });
+    while (!q.empty()) {
+        int x = q.top().first;
+        int dis = -q.top().second;
+        q.pop();
+ 
+        if (d[x] < dis) continue;
+ 
+        for (int i = 0; i < list[x].size(); i++) {
+            int next = list[x][i].first;
+            int nextdis = dis + list[x][i].second;
+ 
+            if (d[next] > nextdis) {
+                d[next] = nextdis;
+                q.push({ next, -nextdis });
+            }
+        }
+    }
+    if (d[des] == INF) cout << "-1\n";
+    else cout << d[des] << '\n';
+}
+ 
+void init() {
+    for (int i = 1; i <= N; i++) {
+        d[i] = INF;
+    }
+}
+ 
+void input() {
+    int u, v;
+    cin >> start >> des >> N >> M;
+    init();
+ 
+    while (M--) {
+        cin >> u >> v;
+        list[u].push_back({ v,1 });
+        list[v].push_back({ u,1 });
+    }
+}
+ 
+int main() {
 
-int node[MAX][MAX];
-bool visited[MAX];
-int dist[MAX];
-int n, m;
-int a, b;
-
-void MakeTable() {
-	for (int i = 1; i <= n; i++)
-	{
-		for (int j = 1; j <= n; j++)
-		{
-			if (i == j) node[i][j] = 0;
-			else node[i][j] = INF;
-		}
-	}
+ 
+    input();
+    dijkstra();
+ 
+    return 0;
 }
 
-void Input() {
-	
-	cin >> a >> b >> n >> m;
-	int x, y;
-	MakeTable();
-	for (int i = 0; i < m; i++)
-	{
-		cin >> x >> y;
-		node[x][y] = 1;
-		node[y][x] = 1;
-	}
-}
-
-
-int GetSmallDist(){
-	int nodenum = 0;
-	int min = INF;
-	for(int i=1;i<=n;i++){
-		if(min> dist[i] && !visited[i])
-		{
-			min = dist[i];
-			nodenum = i;
-		}
-	}
-	return nodenum;
-}
-
-int dihkstra(int n) {
-	int sum = 0;
-	for (int i = 1; i <= n; i++)
-	{
-		dist[i] = node[n][i];
-	}
-	visited[n] = true;
-	for(int i=1; i<=n-2; i++)
-	{
-		int current = GetSmallDist();
-		visited[current] = true;
-		for(int j = 1;j<=n ; j++)
-		{
-			if(!visited[j])
-			{
-				if(dist[j] > node[current][j] + dist[current])
-					dist[j] = node[current][j] + dist[current];
-			}
-		}
-	}
-	return node[a][b];
-}
-
-int main(void){
-	int sol;
-	Input();
-	sol = dihkstra(a);
-	cout << sol;
-}
 
 //https://www.acmicpc.net/problem/14496
 //다익스트라 알고리즘 공부 필요함
