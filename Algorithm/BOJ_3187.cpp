690eba865998d19ec04d7276c509ef07d06c2188
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2021-07-30 12:38:07 +0900

    BFS / 난이도★

--- Algorithm/BOJ_3187.cpp
+++ Algorithm/BOJ_3187.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<queue>
+#include<string.h>
 #define MAX 250
 
 using namespace std;
@@ -34,11 +35,20 @@ void bfs(int a, int b)
 	queue<pair<int, int>>q;
 	q.push(make_pair(a, b));
 	visited[a][b] = true;
+
 	while (!q.empty())
 	{
 		int y = q.front().first;
 		int x = q.front().second;
 		q.pop();
+		if (field[y][x] == 'v')
+		{
+			vk[0][0]++;
+		}
+		else if (field[y][x] == 'k')
+		{
+			vk[1][0]++;
+		}
 		for (int i = 0; i < 4; i++)
 		{
 			int nx = x + dx[i];
@@ -49,34 +59,26 @@ void bfs(int a, int b)
 			if (field[ny][nx] != '#' && !visited[ny][nx])
 			{
 				visited[ny][nx] = true;
-				cout << field[ny][nx] << "\n";
-				if (field[ny][nx] == 'v')
-				{
-					vk[0][0]++;
-				}
-				if (field[ny][nx] == 'k')
-				{
-					vk[1][0]++;
-				}
 				q.push(make_pair(ny, nx));
 			}
 		}
 	}
 
-	cout << vk[0][0] << " " << vk[0][1] << " " << vk[1][0] << " " << vk[1][1] << "\n";
+	
 	if (vk[0][0] < vk[1][0])
 		vk[0][0] = 0;
-	else
-		vk[0][0] = 0;
+	else if (vk[0][0] >= vk[1][0])
+		vk[1][0] = 0;
 	vk[0][1] += vk[0][0];
 	vk[1][1] += vk[1][0];
+	vk[0][0] = 0;
+	vk[1][0] = 0;
 }
 
 
 int main() {
 	memset(field, '#', sizeof(field));
 	Input();
-	int max = 0;
 
 	for (int i = 0; i < n; i++)
 	{
@@ -88,6 +90,8 @@ int main() {
 			}
 		}
 	}
-	cout << vk[0][1] << " " << vk[1][1];
+	cout << vk[1][1] << " " << vk[0][1];
 }
-//https://www.acmicpc.net/problem/3187
+//https://www.acmicpc.net/problem/3187 
+//BFS 풀이 / BFS 함수에서 시작 (y, x) 좌표 누락 수정, 출력 순서 수정 후 정답처리
+//vk함수는 BFS에서 양과 늑대의 숫자 카운팅과 토탈 숫자 카운팅을 담기 위한 함수
