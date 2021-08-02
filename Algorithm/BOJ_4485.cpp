1f4dc88800d2bff8eb7c265ff349bc67213ff3c3
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2021-08-02 23:00:34 +0900

    Dijkstra / 난이도 ★★

--- Algorithm/BOJ_4485.cpp
+++ Algorithm/BOJ_4485.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 #include<queue>
 #include<vector>
-#define MAX 125
+#include<cstring>
+#define MAX 126
 #define INF 987654321
 
 using namespace std;
@@ -33,21 +34,9 @@ bool Input() {
 		return true;
 	}
 }
-
-void init(int a, int b) {
-	node[a][b] = dist[a][b];
-	for (int k = 0; k < 4; k++)
-	{
-		int y = a + dy[k];
-		int x = b + dx[k];
-
-		if (y < 1 || y > n || x < 1 || x > n) continue;
-		node[y][x] = dist[y][x];
-	}
-}
-
 void make_connect() {
 	node[1][1] = dist[1][1];
+	
 	for (int i = 1; i <= n; i++)
 	{
 		for (int j = 1; j <= n; j++)
@@ -58,6 +47,7 @@ void make_connect() {
 				int x = j + dx[k];
 				if (y == 1 && x == 1) continue;
 				if (y < 1 || y > n || x < 1 || x > n) continue;
+				//connect[i][j].clear;
 				connect[i][j].push_back(make_pair(make_pair(y, x),dist[y][x]));
 			}
 		}
@@ -83,7 +73,7 @@ void dijkstra(int a, int b)
 			int nextX = connect[currentY][currentX][i].first.second;
 			int next_dist = distance + connect[currentY][currentX][i].second;
 
-			if (dist[nextY][nextX] > next_dist)
+			if (node[nextY][nextX] > next_dist)
 			{
 				node[nextY][nextX] = next_dist;
 				q.push(make_pair(make_pair(nextY, nextX), -1 * next_dist));
@@ -98,21 +88,15 @@ int main(void) {
 	int cnt = 0;
 	while (Input())
 	{
+		memset(connect, 0, sizeof(connect));
 		cnt++;
-		init(1, 1);
+		node[1][1] = dist[1][1];
 		make_connect();
 		dijkstra(1, 1);
-		for (int i = 1; i <= n; i++)
-		{
-			for (int j = 1; j <= n; j++)
-			{
-				
-				cout << node[i][j] << " " ;
-			}
-			cout << "\n";
-		}
 		cout << "Problem "<< cnt << ": " << node[n][n] << "\n";
 	}
 }
 //https://www.acmicpc.net/problem/4485
 //다익스트라로 구현 시도
+//210802 예제 출력에 문제가 발생해서 손코딩으로 문제를 3시간동안 찾았는데 안나와서 디버깅으로 찾으니 벡터 초기화를 안함...
+//벡터 초기화구문 넣고 init() 삭제 후 정답처리 받음
