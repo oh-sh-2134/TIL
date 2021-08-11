fc8359510eddbaea34d5bb62ae203131197dd41f
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2021-08-11 13:07:58 +0900

    Add files via upload

---
+++ Algorithm/Codility_lesson_2_1.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+vector<int> solution(vector<int> &A, int K) {
+	// write your code in C++14 (g++ 6.2.0)
+	vector<int>::iterator it;
+
+	if (A.empty())
+	{
+		return A:
+	}
+		
+
+	while (K--) {
+
+		it = A.begin();
+		A.insert(it, A.back());
+		A.pop_back();
+
+	}
+	return A;
+
+
+}
+
+
+int main(void)
+{
+	vector<int> v;
+	//v.push_back(1);
+	//v.push_back(2);
+	//v.push_back(3);
+	//v.push_back(4);
+
+	solution(v, 3);
+}
+
+//https://app.codility.com/demo/results/trainingT5GDMG-99V/
+//벡터가 비어 있는 케이스가 빠짐
+
+//https://app.codility.com/demo/results/trainingQ84FK9-BXT/
+//100점 확인
