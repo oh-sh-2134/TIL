538cc91d8d801b44cb111484f94b7313b6e84ac1
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2021-09-05 20:29:15 +0900

    Update Kakao2020_1.cpp

--- Algorithm/Kakao2020_1.cpp
+++ Algorithm/Kakao2020_1.cpp
@@ -1,28 +1,58 @@
 #include <string>
 #include <vector>
-#include <iostream>
-
+#include<algorithm>
+#include<iostream>
 using namespace std;
 
 int solution(string s) {
-	int n = s.length();
-	int cnt = 0, i = 0;
-	for (int k = 1; k <= n / 2; k++) { // °°À» ¼ö ÀÖ´Â ¹®ÀÚ¿­ÀÇ ÃÑ±æÀÌ
-		if (i + k > n) { // ¹üÀ§°¡ ¹þ¾î³ª¹ö¸²
-			cnt += n - i;
-			break;
+	int answer = s.length();
+	for (int i = 1; i <= s.length() / 2; i++)
+	{
+		int pos = 0;
+		int len = s.length();
+		for (; pos + i <= s.length();)
+		{
+			string unit = s.substr(pos, i);
+			pos += i;
+			
+			int cnt = 0;
+			
+			for (; pos + i <= s.length();) 
+			{
+				if (unit == s.substr(pos, i))
+				{
+					cnt++;
+					pos += i;
+				}
+				else
+					break;
+			}
+			//ì••ì¶•ì´ ê°€ëŠ¥í• ë•Œ ë¬¸ìžì—´ì˜ í¬ê¸°ë¥¼ ì¤„ìž„
+			if (cnt > 0)
+			{
+				len -= i * cnt;
+				if (cnt < 9)
+					len += 1;
+				else if (cnt < 99)
+					len += 2;
+				else if (cnt < 999)
+					len += 3;
+				else
+					len += 4;
+
+			}
+			answer = min(answer, len);
 		}
-		// ºñ±³ÇÏ´Â ºÎºÐ °í¹Î
-	
 	}
-	int answer = 0;
 	return answer;
 }
 
-int main(void) {
-	string s = "aabbaccc"; //7
-	cout << solution(s);
-
+int main(void)
+{
+	string s1 = "aabbaccc";
+	string s2= "abcabcabcabcdededededede";
+	string s3 = "ababcdcdababcdcd";
+	cout << solution(s3);
 }
 //https://tech.kakao.com/2019/10/02/kakao-blind-recruitment-2020-round1/
-//¹®ÀÚ¿­ÀÇ ±æÀÌ°¡ nÀÏ¶§ 2/n º¸´Ù Å©°Ô Àß¶ó¾ß ÇÔ
+//ë¬¸ìžì—´ì˜ ê¸¸ì´ê°€ nì¼ë•Œ 2/n ë³´ë‹¤ í¬ê²Œ ìž˜ë¼ì•¼ í•¨
