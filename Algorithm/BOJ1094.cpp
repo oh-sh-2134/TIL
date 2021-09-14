414c84e836b047ad000873dfeea83cbd62f02ce4
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2021-09-14 13:20:18 +0900

    Update BOJ1094.cpp

--- Algorithm/BOJ1094.cpp
+++ Algorithm/BOJ1094.cpp
@@ -6,12 +6,15 @@ int main() {
 	int X;
 	cin >> X;
 	int ans = 0;
-	while (X > 0) 
+	while (X > 0)
 	{
-		if (X % 2 == 1) 
+		if (X & 1)
 			ans++;
 
-		X /= 2;
+		X >>= 1;
 	}
 	cout << ans;
 }
+
+//https://www.acmicpc.net/problem/1094
+//비트문제는 비트답게 풀어야 제맛
