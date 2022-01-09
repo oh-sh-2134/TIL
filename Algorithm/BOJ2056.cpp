2ea6d6b5ebca497331f3ca0a66a7c332e11d384b
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-01-09 23:35:59 +0900

    위성정렬/난이도★★

--- Algorithm/BOJ2056.cpp
+++ Algorithm/BOJ2056.cpp
@@ -10,8 +10,9 @@ int N, M;
 int sol;
 int indegree[MAX];
 int time[MAX];
-vector<int> work[MAX];
+vector<pair<int,int>> work[MAX];
 
+int max(int a, int b) {	return a > b ? a : b; }
 
 void Input() {
 	int a;
@@ -22,7 +23,7 @@ void Input() {
 		while (M--) {
 			cin >> a;
 			indegree[i]++;
-			work[a].push_back(i);
+			work[a].push_back({ i,time[i] });
 		}
 	}
 }
@@ -37,11 +38,15 @@ void solution() {
 	while (!q.empty()) {
 		int current = q.front();
 		q.pop();
-		sol += time[current];
+		sol = max(sol, time[current]);
 		for (auto itor : work[current]) {
-			int next = itor;
-			if (--indegree[next] == 0)
+			int next = itor.first;
+			int next_time = itor.second;
+			time[next] = max(time[next], time[current] + next_time);
+
+			if (--indegree[next] == 0) {
 				q.push(next);
+			}
 		}
 	}
 }
@@ -51,3 +56,26 @@ int main(void) {
 	solution();
 	cout << sol;
 }
+/*
+1번 반례
+작업의 우선순의가 없을때
+7
+5 0
+3 0
+6 0
+1 0
+8 0
+4 0
+2 0
+8
+
+2번 반례
+큐에 넣기전에 max 비교를 하게되면 이전 작업이 오래 걸리는 케이스에서 문제발생함
+5
+6 0
+3 0
+3 2 1 2
+1 1 1
+1 2 3 4
+
+*/
