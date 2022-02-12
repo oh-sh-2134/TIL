0a49731f8afebcf5f6192b6eeec2ff5ff41e3136
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-02-12 22:19:44 +0900

    sort/난이도★

---
+++ Algorithm/Programers_H_Index.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> citations) {
+	int answer = 0;
+
+	sort(citations.begin(), citations.end());
+
+	for (int i = 0; i < citations.size(); i++) {
+		if (citations[i] >= citations.size() - i) return (citations.size() - i);
+	}
+	return answer;
+}
