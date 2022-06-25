9d7fc740063ba6d1aebefb244b09f3e3094c5c2b
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-06-25 10:52:37 +0900

    Greedy/난이도★

---
+++ Algorithm/Programers_큰 수 만들기.cpp
@@ -0,0 +1,34 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string solution(string number, int k) {
+	int cnt = k;
+	int maxPos = 0;
+	int start = 0;
+	string answer = "";
+	while (cnt>0) {
+		int frontMax = 0;
+		int cnt2 = cnt;
+		if (answer.length() + cnt == number.length()) break;
+		//앞에서 가장 큰수를 찾는다
+		for (int i = start; cnt2>=0; cnt2--,i++) {
+			if (frontMax<int(number[i])) {
+				frontMax = int(number[i]);
+				maxPos = i;
+			}			
+		}
+		cnt -= (maxPos - start);
+		answer += number[maxPos];
+		if (cnt == 0 && (number.length() - answer.length() != k)) answer += number.substr(maxPos + 1);
+		start = maxPos+1;
+	}
+	return answer;
+}
+
+int main(void) {
+	string s = "654321";
+	solution(s, 5);
+}
+//https://programmers.co.kr/learn/courses/30/lessons/42883
