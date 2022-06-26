cd945b40e530ae09917ca070854187f7fb603b6d
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-06-26 21:58:29 +0900

    Greedy/난이도★

---
+++ Algorithm/Programers_구명보트.cpp
@@ -0,0 +1,24 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+	int answer = 0;
+	sort(people.begin(), people.end());
+	int together = 0;
+	//같이 탈수 있었던 사람의 수와 people에 남은 수가 같으면 보트를 다 탄거임
+	while (together < people.size()) {
+		//같이 탈수 있으면(2명까지만 탈 수 있음)
+		if (people[together] + people.back() <= limit) {
+			together++;
+		}
+		people.pop_back();
+		answer++;
+
+	}
+
+
+	return answer;
+}
+//https://programmers.co.kr/learn/courses/30/lessons/42885
