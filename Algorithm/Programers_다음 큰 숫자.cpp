80d051092e37e6e765704dc5b7021275691772d6
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-06-27 22:35:13 +0900

    Greedy/난이도★

---
+++ Algorithm/Programers_다음 큰 숫자.cpp
@@ -0,0 +1,27 @@
+﻿#include <string>
+#include <vector>
+using namespace std;
+
+//bit 개수 return
+int getBitCount(int n) {
+	int bitCnt = 0;
+	while (n) {
+		if (n % 2)
+			bitCnt++;
+		n /= 2;
+	}
+	return bitCnt;
+}
+
+int solution(int n) {
+	int bitCnt = getBitCount(n);
+	//비트 개수가 같은 수를 찾을때까지 반복
+	while (1) {
+		n++;
+		if (bitCnt == getBitCount(n))
+			break;
+	}
+	int answer = n;
+	return answer;
+}
+//https://programmers.co.kr/learn/courses/30/lessons/12911
