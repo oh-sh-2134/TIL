286a6e5c13fd270e8160df63df2a211423720d79
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-08-03 23:42:19 +0900

    Greedy / 난이도★

---
+++ Algorithm/Programers_체육복.cpp
@@ -0,0 +1,24 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int student[30];
+
+int solution(int n, vector<int> lost, vector<int> reserve) {
+	int answer = 0;
+	for (auto iter : lost)
+		student[iter]--;
+	for (auto iter : reserve)
+		student[iter]++;
+	for (int i = 1; i <= n; i++) {
+		if (student[i] == -1) {
+			if (student[i - 1] == 1)
+				student[i - 1] = student[i] = 0;
+			else if (student[i + 1] == 1)
+				student[i + 1] = student[i] = 0;
+		}
+		if (student[i] >= 0)answer++;
+	}
+	return answer;
+}
+//https://school.programmers.co.kr/learn/courses/30/lessons/42862
