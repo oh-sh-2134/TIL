a7a7c9e9e098c1a1eb34127a4575a21da5e5ab07
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-08-14 00:00:05 +0900

    BinarySearch / 난이도

---
+++ Algorithm/BOJ_12015.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int n;
+vector<int> arr;
+vector<int> v;
+void input() {
+	cin >> n;
+	int k;
+	for (int i = 0; i < n; i++) {
+		cin >> k;
+		arr.push_back(k);
+	}
+}
+
+int lowerBound(int idx, int val) {
+	int lo = 0, hi = idx;
+	int rst = idx;
+	while (lo <= hi) {
+		int mid = (lo + hi) / 2;
+		if (v[mid] >= val) {
+			lo = mid - 1;
+			rst = mid;
+		}
+		else {
+			hi = mid + 1;
+		}
+	}
+	return rst;
+}
+void solution() {
+	for (int i = 0; i < n; i++) {
+		if (v.empty()) { 
+			v.push_back(arr[i]); 
+			continue; 
+		}
+		if (v.back() < arr[i]) {
+			v.push_back(arr[i]);
+		}
+		else {
+			v[lowerBound(v.size()-1, arr[i])] = arr[i];
+		}
+	}
+	cout << v.size();
+
+}
+
+int main(void) {
+	input();
+	solution();
+}
+//https://www.acmicpc.net/problem/12015
