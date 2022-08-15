fd9b17bc20a55dc7b5ebf4a234cf9e3481690fb0
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-08-15 23:50:31 +0900

    Greedy? / 난이도★

---
+++ Algorithm/Programers_더 개 뽑아서 더하기.cpp
@@ -0,0 +1,20 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+	vector<int> answer;
+	//set은 insert마다 sort 하기 때문에 마지막 vector에서 sort하기 위해 unordered_set사용
+	unordered_set<int> us;
+	for (int i = 0; i < numbers.size(); i++) {
+		for (int j = i + 1; j < numbers.size(); j++) {
+			us.insert(numbers[i] + numbers[j]);
+		}
+	}
+	answer.assign(us.begin(), us.end());
+	sort(answer.begin(), answer.end());
+	return answer;
+}
+//https://school.programmers.co.kr/learn/courses/30/lessons/68644
