d2f9f90641da3c71102c85dbf5b7172cb6ca55ab
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-08-16 23:00:52 +0900

    BinartSearch / 난이도★★
    규칙을 찾는것이 어려움

---
+++ Algorithm/BOJ_1300.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+using namespace std;
+
+long long n, k;
+
+void input() {
+	cin >> n >> k;
+}
+
+
+/*
+n*n에서 num보다 작은 숫자의 개수를 구하는 함수
+*/
+long long findLessNun(long long num) {
+	long long cnt = 0;
+	for (long long i = 1; i <= n; i++) {
+		cnt += min(n, ((long long)num / i));		
+		if (i > num) break;
+	}
+	return cnt;
+}
+
+void solution(){
+	long long lo = 1, hi = n * n;
+	long long sol = 0;
+	while (lo <= hi) {
+		long long mid = (lo + hi) / 2;
+		if (findLessNun(mid)<k) {
+			lo = mid + 1;
+		}
+		else {
+			sol = mid;
+			hi = mid - 1;
+		}
+	}
+	cout << sol;
+}
+
+int main(void) {
+	input();
+	solution();
+}
+//https://www.acmicpc.net/problem/1300
