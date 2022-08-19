36450cd94459f828ad2dbc0d4b36d403f12b67bd
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-08-19 23:05:01 +0900

    Greedy / 난이도★

---
+++ Algorithm/Programers_올바른 괄호.cpp
@@ -0,0 +1,27 @@
+﻿#include<string>
+
+using namespace std;
+
+bool solution(string s)
+{
+	bool answer = true;
+	int cnt = 0;
+	for (auto iter : s) {
+		if (iter == '(') {
+			cnt++;
+		}
+		else {
+			cnt--;
+		}
+		//열기 전에 닫기가 나온 케이스
+		if (cnt < 0) {
+			return false;
+		}
+	}
+	//열린 상태로 끝나는 케이스
+	if (cnt)
+		return false;
+
+	return answer;
+}
+//https://school.programmers.co.kr/learn/courses/30/lessons/12909
