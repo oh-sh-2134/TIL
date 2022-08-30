d8acf5580a8817d879d994a73436138e1f07203b
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-08-30 22:55:37 +0900

    Stack / 난이도★

---
+++ Algorithm/Programers_같은 숫자는 싫어.cpp
@@ -0,0 +1,17 @@
+﻿#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> solution(vector<int> arr)
+{
+	vector<int> answer;
+	for (auto iter : arr) {
+		if (answer.size() == 0) answer.push_back(iter);
+		if (answer.back() == iter) continue;
+		answer.push_back(iter);
+	}
+
+
+	return answer;
+}
+//https://school.programmers.co.kr/learn/courses/30/lessons/12906
