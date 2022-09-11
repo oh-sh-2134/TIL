f5f1d3365064faa2741e734296aae5d32e6d22e4
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-09-11 12:11:51 +0900

    BinarySearch + Dijkstra / 난이도★★★
    매개변수 탐색 문제 상당히 어려움

--- Algorithm/BOJ_1800.cpp
+++ Algorithm/BOJ_1800.cpp
@@ -1,15 +1,15 @@
-﻿#include <iostream>
+#include <iostream>
 #include <queue>
 #include <algorithm>
-
+#define MAX 987654321
 using namespace std;
 
-int N, P, K, maxCost, sol;
+int N, P, K, maxCost;
 vector<vector<pair<int,int>>> node;
 void input() {
 	cin >> N >> P >> K;
 	node.resize(N + 1);
-	for (int i = 0; i < N; i++) {
+	for (int i = 0; i < P; i++) {
 		int a, b, c;
 		cin >> a >> b >> c;
 		node[a].push_back({ b,c });
@@ -19,36 +19,34 @@ void input() {
 }
 
 bool dijkstra(int target) {
-	vector<int> cost(N + 1, N);
+	vector<int> cost(N + 1, MAX);
 	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 	pq.push({ 0, 1 });
-
+	cost[1] = 0;
 	while (!pq.empty()) {
 		int currentNode = pq.top().second;
 		int currentCost = pq.top().first;
 		pq.pop();
-		if (cost[currentNode] <= currentCost)  continue;
-		cost[currentNode] = currentCost;
+		if (cost[currentNode] < currentCost)  continue;
 
 		for (int i = 0; i < node[currentNode].size(); i++) {
 			int nextNode = node[currentNode][i].first;
-			int nextCost = currentCost + (node[currentNode][i].second > target);
-			if (nextCost < cost[currentNode]) {
-				cost[currentNode] = nextCost;
-				pq.push({ nextNode ,nextCost });
+			// 최소 가중치를 넘는 간선에는 1부여 아닐 경우 0으로 표시
+			int nextCost = currentCost + (node[currentNode][i].second > target); 
+			if (nextCost < cost[nextNode]) {
+				cost[nextNode] = nextCost;
+				pq.push({ nextCost ,nextNode });
 			}
 		}
 	}
-	return (cost[N] <= K);
+	return (cost[N] <= K); // 최소 가중치를 넘는 간선이 K개를 넘는지 확인
 }
-
-void binarySearch() {
-	int l, r, mid;
-	l = 0;
-	r = maxCost;
-	sol = -1;
+//매개변수 탐색 - 목표 최소 비용 결정
+int binarySearch(int l, int r) {
+	int mid;
+	int sol = -1;
 	while (l <= r) {
-		mid = l + (r - l) / 2;
+		mid = (l + r) / 2;
 		if (dijkstra(mid)) {
 			r = mid - 1;
 			sol = mid;
@@ -56,11 +54,12 @@ void binarySearch() {
 		else
 			l = mid + 1;
 	}
+	return sol;
 }
 
+//특정 가중치 이하의 간선으로만 경로를 구성할 수 있는지 판단해야 하는 문제
 void solution() {
-	binarySearch();
-	cout << sol;
+	cout << binarySearch(0, maxCost);
 }
 int main(void) {
 	input();
