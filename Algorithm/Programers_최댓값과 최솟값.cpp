32b8d696459b29b7a66f92a5d9cc4ac173916ec4
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-09-12 23:52:50 +0900

    Greedy (stringstream 사용) / 난이도★

---
+++ Algorithm/Programers_최댓값과 최솟값.cpp
@@ -0,0 +1,18 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
+string solution(string s) {
+	string answer = "", token = "";
+	vector<int> v;
+
+	for (stringstream str(s); str >> token;)
+		v.push_back(stoi(token));
+	sort(v.begin(), v.end());
+
+	answer += to_string(v.front()) + ' ' + to_string(v.back());
+	return answer;
+}
