263028081d28e435711d9f3265410bee9608bf8d
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-10-10 21:20:14 +0900

    Greedy / 난이도★

---
+++ Algorithm/Programers_짝수와 홀수.cpp
@@ -0,0 +1,13 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int num) {
+	string answer = "Even";
+	if (num & 1)
+		answer = "Odd";
+
+	return answer;
+}
+//https://school.programmers.co.kr/learn/courses/30/lessons/12937
