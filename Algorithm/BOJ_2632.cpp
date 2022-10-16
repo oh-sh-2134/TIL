fe9888e2c753cae0d200ba218e83c906c2f485a9
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-10-16 22:21:07 +0900

    Add files via upload

---
+++ Algorithm/BOJ_2632.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> arr;
+vector<bool> visited;
+int n;
+int a, b;
+int cnt;
+
+
+void input() {
+	cin >> n;
+	cin >> a >> b;
+	for (int i = 0; i < a; i++) {
+		int p;
+		cin >> p;
+		arr.push_back(p);
+	}
+	for (int i = 0; i < b; i++) {
+		int p;
+		cin >> p;
+		arr.push_back(p);
+	}
+	visited.resize(a + b, false);
+}
+
+void dfs(int cur) {
+	if (cur > n) return;
+	if (cur == n) {
+		cnt++; 
+		return;
+	}
+	for (int i = 0; i < a + b; i++) {
+		if (visited[i]) continue;
+		visited[i] = true;
+		dfs(arr[i]+cur);
+		visited[i] = false;
+	}
+}
+
+void solution() {
+	for (int i = 0; i < a + b; i++) {
+		visited[i] = true;
+		dfs(arr[i]);
+		visited[i] = false;
+	}
+	cout << cnt;
+}
+
+int main(void) {
+	input();
+	solution();
+}
+//https://www.acmicpc.net/problem/2632
