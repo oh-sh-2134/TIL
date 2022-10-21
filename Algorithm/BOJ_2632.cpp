7b36d49a98af35cc4973dcfa112d85594da1b282
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-10-21 23:45:19 +0900

    Prefix Sum / 난이도★★

--- Algorithm/BOJ_2632.cpp
+++ Algorithm/BOJ_2632.cpp
@@ -5,7 +5,9 @@ using namespace std;
 
 vector<int> pizzaA;
 vector<int> pizzaB;
+//A에서 나올 수 있는 합
 vector<int> cntA;
+//B에서 나올 수 있는 합
 vector<int> cntB;
 
 int n;
@@ -28,18 +30,25 @@ void input() {
 	cntB.resize(2000001);
 }
 
-void func(int p, vector<int> arr, vector<int> arrCnt) {
+void func(int p, vector<int> &arr, vector<int> &arrCnt) {
+	//뒤로 늘려감
 	for (int i = 1; i <= p; i++) {
 		int sum = 0;
-		for (int j = 0; j < i; j++) sum += arr[j];
+		for (int j = 0; j < i; j++) {
+			sum += arr[j];
+		}
 		arrCnt[sum]++;
+		
+		//피자 하나에서만 가져오는 케이스
 		if (sum == n) cnt++;
 
 		if (i == p) break;
+		//앞에서 줄여감
 		for (int j = 1; j < p; j++) {
 			sum -= arr[j - 1];
 			sum += arr[(j + i - 1) % p];
 			arrCnt[sum]++;
+			//피자 하나에서만 가져오는 케이스 
 			if (sum == n) cnt++;
 		}
 	}
@@ -47,8 +56,10 @@ void func(int p, vector<int> arr, vector<int> arrCnt) {
 
 
 void solution() {
-	func(a, pizzaA, cntA);
-	func(b, pizzaB, cntB);
+	// a에서의 연속된 피자의 합
+	func(a, pizzaA, cntA); 
+	// b에서의 연속된 피자의 합
+	func(b, pizzaB, cntB); 
 	for (int i = 1; i < n; i++) {
 		int j = n - i;
 		cnt += cntA[i] * cntB[j];
