a9864ca76702608fe46e15e223ffc9563d7fad57
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-10-22 23:27:46 +0900

    Add files via upload

---
+++ Algorithm/Programers_숫자의 표현.cpp
@@ -0,0 +1,20 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+	int answer = 1;
+	for (int i = 1; i < n; i++) {
+		int sum = i, cur = i;
+		while (sum < n) {
+			sum += ++cur;
+			if (sum == n) {
+				answer++;
+				break;
+			}
+		}
+	}
+	return answer;
+}
+//https://school.programmers.co.kr/learn/courses/30/lessons/12924
