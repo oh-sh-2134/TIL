8015eeb77f13711e4bb3ab22acd49c4c74553dd4
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-10-25 23:37:58 +0900

    DP/ 난이도★★

---
+++ Algorithm/Programers_멀리 뛰기.cpp
@@ -0,0 +1,17 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int n) {
+	vector<long long> dp(n + 1, 0);
+	dp[0] = 1;
+	dp[1] = 2;
+	for (int i = 2; i < n; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2];
+		dp[i] = dp[i] % 1234567;
+	}
+	long long answer = dp[n - 1];
+	return answer;
+}
+//https://school.programmers.co.kr/learn/courses/30/lessons/12914
