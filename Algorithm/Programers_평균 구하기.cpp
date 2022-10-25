008e20c105df3bf9a44fd6d0d846074c078d73c1
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-10-26 00:16:01 +0900

    Add files via upload

---
+++ Algorithm/Programers_평균 구하기.cpp
@@ -0,0 +1,11 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+	double answer = 0;
+	for (auto e : arr) { answer += e; }
+	return answer / arr.size();
+}
+//https://school.programmers.co.kr/learn/courses/30/lessons/12944
