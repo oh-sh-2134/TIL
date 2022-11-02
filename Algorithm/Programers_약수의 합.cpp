53bff41bc91caff3605370fe2b94c64699e7e62c
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-11-02 23:17:57 +0900

    Add files via upload

---
+++ Algorithm/Programers_약수의 합.cpp
@@ -0,0 +1,14 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+	int answer = n;
+	for (int i = 1; i <= n / 2; i++) {
+		if (n%i == 0)
+			answer += i;
+	}
+	return answer;
+}
+//https://school.programmers.co.kr/learn/courses/30/lessons/12928
