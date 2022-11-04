6e409da6d21d8c93eacae5fd8da706fafb1d87bb
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-11-04 23:55:54 +0900

    Add files via upload

---
+++ Algorithm/Programers_할인 행사.cpp
@@ -0,0 +1,33 @@
+﻿#include <string>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+unordered_map<string, int> map;
+bool check() {
+	for (auto &iter : map) {
+		if (iter.second)
+			return false;
+	}
+	return true;
+}
+
+int solution(vector<string> want, vector<int> number, vector<string> discount) {
+	int answer = 0;
+
+	for (int i = 0; i < want.size(); i++)
+		map[want[i]] = number[i];
+
+	for (int i = 0; i < discount.size(); i++) {
+		map[discount[i]]--;
+		if (i < 10) continue;
+		if (map.find(discount[i - 10]) != map.end()) {
+			map[discount[i - 10]]++;
+		}
+		if (check())
+			answer++;
+
+	}
+	return answer;
+}
+//https://school.programmers.co.kr/learn/courses/30/lessons/131127
