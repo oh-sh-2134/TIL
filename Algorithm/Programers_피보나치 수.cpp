98dc6eed7e715262329716a7653a05cbd2761f0c
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-11-13 18:16:23 +0900

    DP / 난이도★

---
+++ Algorithm/Programers_피보나치 수.cpp
@@ -0,0 +1,15 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+	vector<int> v(n + 1);
+	v[0] = 0;
+	v[1] = 1;
+	for (int i = 2; i <= n; i++) {
+		v[i] = v[i - 1] + v[i - 2];
+		v[i] %= 1234567;
+	}
+	return v[n];
+}
