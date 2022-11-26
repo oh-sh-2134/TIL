c0bc377be725ac14966f8fd8aaf3dc63b39fcf35
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-11-26 10:53:30 +0900

    Add files via upload

---
+++ Algorithm/Programers_N개의 최소공배수.cpp
@@ -0,0 +1,31 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int Gcd(int a, int b)
+{
+	int A = max(a, b);
+	int B = min(a, b);
+
+	while (A % B != 0)
+	{
+		int R = A % B;
+		A = B;
+		B = R;
+	}
+	return B;
+}
+
+int solution(vector<int> arr)
+{
+	int answer = arr[0];
+	for (int i = 1; i < arr.size(); i++)
+	{
+		int GCD = Gcd(answer, arr[i]);
+		int LCM = answer * arr[i] / GCD;
+		answer = LCM;
+	}
+	return answer;
+}
+//https://school.programmers.co.kr/learn/courses/30/lessons/12953
