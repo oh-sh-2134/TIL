e851ef0fc1688b476494f5e324557186f657bcb4
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-12-02 22:40:17 +0900

    Queue / 난이도★★

---
+++ Algorithm/Programers_야근 지수.cpp
@@ -0,0 +1,21 @@
+﻿#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+
+long long solution(int n, vector<int> works) {
+	long long answer = 0;
+	priority_queue<int> pq(works.begin(), works.end());
+	while (pq.top()*n) {
+		int i = pq.top() - 1;
+		pq.pop();
+		pq.push(i);
+		n--;
+	}
+	while (!pq.empty()) {
+		answer += pq.top()*pq.top();
+		pq.pop();
+	}
+	return answer;
+}
+//https://school.programmers.co.kr/learn/courses/30/lessons/12927
