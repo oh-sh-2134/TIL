c8ac2e6e0ba936d6f2ddb835bdbeea1e83fec551
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-12-03 12:38:14 +0900

    Greedy / 난이도★★
    Use lambda

---
+++ Algorithm/Programers_단속카메라.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<vector<int>> routes) {
+	int answer = 0;
+	int enterPoint = -30001;
+	sort(routes.begin(), routes.end(),
+		[](vector<int> a, vector<int> b)->bool {
+		return a[1] < b[1];
+	});
+	for (auto &iter : routes) {
+		if (enterPoint < iter[0]) {
+			answer++;
+			enterPoint = iter[1];
+		}
+	}
+
+	return answer;
+}
+//https://school.programmers.co.kr/learn/courses/30/lessons/42884
