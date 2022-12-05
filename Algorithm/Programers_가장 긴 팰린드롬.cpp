adb399798f4ea36da6a60f6eb79bb42b58a23a10
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-12-05 21:09:20 +0900

    Greedy /  난이도★

---
+++ Algorithm/Programers_가장 긴 팰린드롬.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int findPalindrome(string s, int left, int right) {
+
+	while (left >= 0 && right < s.size()) {
+		if (s[left] != s[right]) {
+			break;
+		}
+		left--;
+		right++;
+	}
+	return right - left - 1;
+}
+
+int solution(string s)
+{
+	int answer = 0;
+	for (int i = 0; i < s.size(); i++) {
+		answer = max(answer, findPalindrome(s, i, i + 1));
+		answer = max(answer, findPalindrome(s, i, i));
+	}
+	return answer;
+}
+//https://school.programmers.co.kr/learn/courses/30/lessons/12904
