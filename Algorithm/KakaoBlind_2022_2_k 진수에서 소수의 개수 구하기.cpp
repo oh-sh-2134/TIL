7abd26543802d716db3e698982eed8ddbfa6e964
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-12-09 00:02:52 +0900

    resolve

---
+++ Algorithm/KakaoBlind_2022_2_k 진수에서 소수의 개수 구하기.cpp
@@ -0,0 +1,51 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <iostream>
+using namespace std;
+
+bool isPrime(long long n) {
+	if (n <= 1) {
+		return false;
+	}
+
+	for (long long i = 2; i*i <= n; i++) {
+		if ((n%i) == 0) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+
+string makeJinsu(int n, int k) {
+	string s = "";
+	while (n) {
+		s += to_string(n%k);
+		n /= k;
+	}
+	reverse(s.begin(), s.end());
+	return s;
+}
+
+string convert0toSpace(string s) {
+	for (long long i = 0; i < s.size(); i++) {
+		if (s[i] == '0') {
+			s[i] = ' ';
+		}
+	}
+	return s;
+}
+
+int solution(int n, int k) {
+	int answer = 0;
+	long long token = 0;
+	string s = convert0toSpace(makeJinsu(n, k));
+	for (stringstream str(s); str >> token;) {
+		if (isPrime(token))
+			answer++;
+	}
+	return answer;
+}
