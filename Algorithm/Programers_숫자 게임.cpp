bd39cbd45bfea96176b78e691eef802c24d244d9
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-12-12 22:21:34 +0900

    Greedy / 난이도★

---
+++ Algorithm/Programers_숫자 게임.cpp
@@ -0,0 +1,22 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> A, vector<int> B) {
+	int answer = 0;
+	sort(A.begin(), A.end());
+	sort(B.begin(), B.end());
+	for (int i = 0, j = 0; i < A.size() && j < B.size(); i++, j++) {
+		while (j < B.size()) {
+			if (A[i] < B[j]) {
+				answer++;
+				break;
+			}
+			j++;
+		}
+	}
+
+	return answer;
+}
+//https://school.programmers.co.kr/learn/courses/30/lessons/12987
