2098ab640f0debdd7d5b8200fc3e9ee25b29ae34
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-12-13 23:48:40 +0900

    Add files via upload

---
+++ Algorithm/Programers_예상 대진표.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+	int answer = 0;
+	while (a != b) {
+		a = (a + 1) >> 1;
+		b = (b + 1) >> 1;
+		answer++;
+	}
+	return answer;
+}
+//https://school.programmers.co.kr/learn/courses/30/lessons/12985
