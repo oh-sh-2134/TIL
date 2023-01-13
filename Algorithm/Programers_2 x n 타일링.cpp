1330bd29b679abe19e3d5ad4d0842df73ac06535
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2023-01-13 23:15:11 +0900

    DP / 난이도★

---
+++ Algorithm/Programers_2 x n 타일링.cpp
@@ -0,0 +1,17 @@
+﻿#include <string>
+#include <vector>
+#define mod 1000000007
+using namespace std;
+
+int solution(int n) {
+	vector<int> dp;
+	dp.resize(n + 1);
+	dp[1] = 1;
+	dp[2] = 2;
+	for (int i = 3; i <= n; i++) {
+		dp[i] = (dp[i - 1] + dp[i - 2]) % mod;
+	}
+	int answer = dp[n];
+	return answer;
+}
+//https://school.programmers.co.kr/learn/courses/30/lessons/12900
