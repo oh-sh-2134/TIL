3115cb1f70b65396876f6fd02966a7513b7c9ef7
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2023-01-14 20:50:44 +0900

    Greedy / 난이도★

---
+++ Algorithm/Programers_연속 부분 수열 합의 개수.cpp
@@ -0,0 +1,29 @@
+﻿#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int solution(vector<int> elements) {
+	int answer = 0;
+	vector<int> round(elements);
+	set<int> s;
+	for (int i = 0; i < elements.size() - 1; i++)
+		round.push_back(elements[i]);
+
+	for (int i = 0; i < elements.size(); i++) {
+		vector<int> v;
+		for (int j = 0; j < elements.size(); j++) {
+			int num = 0;
+			for (int k = j; k < i + j + 1; k++)
+				num += round[k];
+
+			v.push_back(num);
+		}
+		for (int j = 0; j < v.size(); j++)
+			s.insert(v[j]);
+	}
+	answer = s.size();
+	return answer;
+}
+//https://school.programmers.co.kr/learn/courses/30/lessons/131701
