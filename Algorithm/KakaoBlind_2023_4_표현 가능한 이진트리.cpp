cb676af6922feb5cafcef330277e6b829c3bc7e0
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2023-01-16 20:58:40 +0900

    BinaryTree / 난이도★★

--- Algorithm/KakaoBlind_2023_4_표현 가능한 이진트리.cpp
+++ Algorithm/KakaoBlind_2023_4_표현 가능한 이진트리.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <stack>
 using namespace std;
 string convertDec2Bin(long long n) {
 	string b = "";
@@ -26,27 +27,31 @@ string makeTree(string s) {
 }
 
 
-bool checkNode(string s) {
-	long long mid = (s.length()-1) / 2;
-	if (s.size() == 3)
-		return ((s[0] - '1') + (s[2] - '1')) <= (s[1] - '1') ? true : false;
+bool checkTree(string& str, int left, int right) {
 	
-	if (checkNode(s.substr(0, mid)) && checkNode(s.substr(mid + 1)))
-		return s[mid] == '1';
-
-	return false;
+	if (left == right) {
+		return true;
+	}
+	int mid = (left + right) >> 1;
+	if (str[mid] == '0') {
+		for (int j = left; j <= right; j++) 
+			if (str[j] == '1') return false;		
+		return true;
+	}
+	return checkTree(str, left, mid - 1) & checkTree(str, mid + 1, right);
 }
 
 vector<int> solution(vector<long long> numbers) {
 	vector<int> answer;
 	for (auto n : numbers) {
-		answer.push_back(checkNode(makeTree(convertDec2Bin(n))));
+		string strbit = makeTree(convertDec2Bin(n));
+		answer.push_back(checkTree(strbit, 0, strbit.length() / 2));
 	}
 	return answer;
 }
 
 int main(void) {
-	//vector<long long> v = { 1610612735 };
-	vector<long long> v = { 63, 111, 95 };
+	vector<long long> v = { 7, 42, 5 };
 	solution(v);
 }
+//https://school.programmers.co.kr/learn/courses/30/lessons/150367
