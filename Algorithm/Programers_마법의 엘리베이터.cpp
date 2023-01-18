c90043edb6a5febda6c26db069155e87b5e28f03
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2023-01-18 23:56:23 +0900

    Greedy / 난이도★

--- Algorithm/Programers_마법의 엘리베이터.cpp
+++ Algorithm/Programers_마법의 엘리베이터.cpp
@@ -1,34 +1,21 @@
 #include <string>
 #include <vector>
-
 using namespace std;
 
 int solution(int storey) {
-	int answer = 0;
-
-	while (storey)
-	{
-		int cur = storey % 10;
-		int next = (storey / 10) % 10;
-
-		if (cur > 5)
-		{
-			answer += 10 - cur;
-			storey += 10;
-		}
-		else if (cur == 5)
-		{
-			answer += 5;
-			storey += next >= 5 ? 10 : 0;
-		}
-		else
-		{
-			answer += cur;
-		}
-
-		storey /= 10;
-	}
-
-	return answer;
+    int answer = 0;
+    while(storey){
+        int idx = storey%10;
+        storey/=10;
+        if(idx>=5){ 
+            idx = 10 -idx;
+            if(idx == 5 && (storey%10) <5)
+                storey-=1;
+            storey += 1;
+            
+        }
+        answer+=idx;
+    }
+    return answer;
 }
 //https://school.programmers.co.kr/learn/courses/30/lessons/148653#
