90cb50b1af8e1758bb0ad8b71f34b208b2d03c3c
oh-sh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2023-02-10 14:23:10 +0900

    String / 난이도★

---
+++ Algorithm/Programers_크기가_작은_부분_문자열.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String t, String p) {
+        int len = p.length();
+        long num = Long.parseLong(p);
+        int result = 0;
+
+        for (int i = 0; i < t.length() - len + 1; i++) {
+            long diff = Long.parseLong(t.substring(i, i + len));
+            if (diff <= num) result++;
+        }
+        return result;
+    }
+}
+
+class main{
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String t = "3141592";
+        String p = "271";
+        System.out.println("sol = " + sol.solution(t,p));
+    }
+
+}
